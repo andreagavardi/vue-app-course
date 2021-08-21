bd05cbb6d1259b41cf4db7f20a91d67340aadbb0
andreagavardi <andreagavardi89@gmail.com>
2021-08-21 11:42:09 +0200

    add cpmments in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import EventList from "../views/EventList.vue";
 
 const routes = [
   {
     path: "/", //url
-    name: "Home", //name
-    component: Home, //view component to render
+    name: "EventList", //name
+    component: EventList, //view component to render
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
+    // route level code-splitting serve per non caricare tutto l'app subito lato client per non avere una bad performance
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
